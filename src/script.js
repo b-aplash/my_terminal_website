4470f66bb91e841963846edbd0330a8022c0fe58
Bhavya Aplash <aplashbhavya@gmail.com>
2023-01-09 19:07:23 -0500

    removing button

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 var commands = document.getElementById("commands");
-var commandline = document.getElementById("commandline");
-var temp_btn = document.getElementById("temp_btn");
+var user_input = document.getElementById("user_input");
 
 temp_btn.addEventListener("click", function(){
     var newCommand = document.createElement("li");
@@ -8,3 +7,23 @@ temp_btn.addEventListener("click", function(){
     commands.appendChild(newCommand);
     commandline.value = "";
 });
+
+function execute(input){
+    var newCommand = document.createElement("li");
+    newCommand.innerHTML = input;
+    commands.appendChild(newCommand);
+}
+
+function key(e){
+    const input = user_input.innerHTML;
+
+    if(e.key == "Enter"){
+        execute(input);
+        user_input.value = "";
+        return;
+    }
+
+    user_input.innerHTML = input + e.key;
+}
+
+document.addEventListener("keypress", key);
