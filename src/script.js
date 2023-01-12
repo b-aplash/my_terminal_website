d364cbaa8c3f041566d6162d1c2f0c419a95185e
Bhavya Aplash <aplashbhavya@gmail.com>
2023-01-12 00:08:05 -0500

    adding commands

--- src/script.js
+++ src/script.js
@@ -7,6 +7,16 @@ function execute(input){
     let output;
     input = input.toLowerCase();
     output = `<div>→ ${input}</div>`;
+
+    if(!COMMANDS.hasOwnProperty(input))
+    {
+        output += `<div> No such command: ${input} </div>`
+    }
+    else
+    {
+        output += `<div> ${COMMANDS[input]} </div>`
+    }
+
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML + output}`;
 }
 
@@ -23,3 +33,19 @@ function key(e){
 }
 
 document.addEventListener("keypress", key);
+
+const COMMANDS = 
+{
+    help:
+        'Supported commands: ["about", "experience", "education", "skills", "contact"]',
+    about:
+        'Hi! I am Bhavya and I am a freshman at Penn State!',
+    skills:
+        'My strongest programming language is Java and I do digital art and design.',
+    experience:
+        'I have worked on many projects related to programming and computer science in high school.',
+    education:
+        'I attended Freehold High School and was a part of the Computer Science Academy.',
+    contact:
+        'Phone: 732-737-1371 | Email: aplashbhavya@gmail.com'
+}
